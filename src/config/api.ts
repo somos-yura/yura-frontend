832130eca28fc8b6cb0dcb91c2893a1873682e8c
YadiraAllauca <yadiraallaucac@gmail.com>
2026-01-24 19:15:59 -0500

    updated api config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,12 @@
 export const config = {
   API_BASE_URL:
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (import.meta as any).env?.API_BASE_URL || 'http://localhost:8000',
+    (import.meta as any).env?.PUBLIC_API_BASE_URL ||
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (import.meta as any).env?.API_BASE_URL ||
+    'http://localhost:8000',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  API_VERSION: (import.meta as any).env?.API_VERSION || 'v1',
+  API_VERSION: (import.meta as any).env?.PUBLIC_API_VERSION || 'v1',
   GOOGLE_CLIENT_ID:
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (import.meta.env as any).PUBLIC_GOOGLE_CLIENT_ID ||
